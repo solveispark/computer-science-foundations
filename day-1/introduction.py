4c7d7a934b5283184047aed997608620972ceef4
maroua <maroualakhfif@gmail.com>
2026-02-09 13:50:20 +0800

    Day 1: setup and introduction program

---
+++ day-1/introduction.py
@@ -0,0 +1,11 @@
+Name = "Maroua"
+Field = "Computer Science"
+Reason = "I want to learn how to think logically and build useful projects."
+Goal = " Build real world applications and a strong portfolio to showcase my skills and knowledge."
+
+print(f"Hello! My name is {Name}.")
+print(f"I am interested in {Field}.")
+print("Why computer science?")
+print(Reason)
+print(f"My goal is to {Goal}")
+print("This is the beginning of my journey.")
